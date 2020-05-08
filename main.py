fix inplace ops for grad computation

main.py
@@ -70,8 +70,10 @@ if os.path.exists(model_save_path):
     print('Restoring trained model from %s' % model_save_path)
     checkpoint = torch.load(model_save_path, map_location=device)
     model.load_state_dict(checkpoint['model'])
-    lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    if 'lr_scheduler' in checkpoint:
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+    if 'optimizer' in checkpoint:
+        optimizer.load_state_dict(checkpoint['optimizer'])
     best_val_mAP = checkpoint['mAP']
     start_epoch = checkpoint['epoch']+1
     N_EPOCHS = start_epoch + N_EPOCHS
@@ -80,12 +82,13 @@ if os.path.exists(model_save_path):
 for epoch in range(start_epoch, N_EPOCHS):
     train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=250)
     _, stats = evaluate(model, val_loader, device=device, print_freq=500, header='Val:')
-    lr_scheduler.step()
+    if epoch < 10: # stop decaying after 15 epochs
+        lr_scheduler.step()
 
     val_mAP = stats['bbox'][1] # AP @ IoU=0.5
     if val_mAP > best_val_mAP:
         best_val_mAP = val_mAP
-        checkpoint = {'model': model.state_dict(), 'optimizer' : optimizer.state_dict(), 'lr_scheduler': lr_scheduler.state_dict(),
+        checkpoint = {'model': model.state_dict(), #'optimizer' : optimizer.state_dict(), 'lr_scheduler': lr_scheduler.state_dict(),
                       'epoch': epoch, 'mAP': val_mAP}
         torch.save(checkpoint, model_save_path)
     

main_joint.py
@@ -47,14 +47,14 @@ USE_OBJECTS = True
 USE_PARTS = True
 RETURN_SEPARATE_TARGETS = True
 
-model_save_path = 'saved_model_joint_%s.pth' % (TRAIN_SPLIT)
-
 ########## Hyperparameters ##########
 LEARNING_RATE = args.learning_rate
 BATCH_SIZE = args.batch_size
 WEIGHT_DECAY = args.weight_decay
 FUSION_THRESH = args.fusion_thresh
 USE_ATTENTION = not args.no_attention
+
+model_save_path = 'saved_model_joint_ft-%.2f_%s.pth' % (FUSION_THRESH, TRAIN_SPLIT)
 
 ########## Data Loaders ##########
 train_loader, val_loader, obj_class2ind, obj_n_classes, part_class2ind, part_n_classes = load_data(DATA_DIR, BATCH_SIZE, TRAIN_SPLIT, VAL_SPLIT,
@@ -74,8 +74,10 @@ if os.path.exists(model_save_path):
     print('Restoring trained model from %s' % model_save_path)
     checkpoint = torch.load(model_save_path, map_location=device)
     model.load_state_dict(checkpoint['model'])
-    lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    if 'lr_scheduler' in checkpoint:
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+    if 'optimizer' in checkpoint:
+        optimizer.load_state_dict(checkpoint['optimizer'])
     best_val_mAP = checkpoint['mAP']
     start_epoch = checkpoint['epoch']+1
     N_EPOCHS = start_epoch + N_EPOCHS
@@ -84,13 +86,14 @@ if os.path.exists(model_save_path):
 for epoch in range(start_epoch, N_EPOCHS):
     train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=250)
     _, _, obj_det_stats, part_det_stats = evaluate(model, val_loader, device=device, print_freq=500, header='Val:')
-    lr_scheduler.step()
+    if epoch < 10: # stop decaying after 10 epochs
+        lr_scheduler.step()
 
     val_obj_det_mAP = obj_det_stats['bbox'][1] # AP @ IoU=0.5
     val_part_det_mAP = part_det_stats['bbox'][1] # AP @ IoU=0.5
     if val_obj_det_mAP > best_val_mAP['OBJECT']: # Save model which performs best for object detection
         best_val_mAP = {'OBJECT': val_obj_det_mAP, 'PART': val_part_det_mAP}
-        checkpoint = {'model': model.state_dict(), 'optimizer' : optimizer.state_dict(), 'lr_scheduler': lr_scheduler.state_dict(),
+        checkpoint = {'model': model.state_dict(), #'optimizer' : optimizer.state_dict(), 'lr_scheduler': lr_scheduler.state_dict(),
                       'epoch': epoch, 'mAP': best_val_mAP}
         torch.save(checkpoint, model_save_path)
     

models.py
@@ -272,7 +272,8 @@ class JointDetector(nn.Module):
         """
         N1, N2 = obj_boxes.shape[0], part_boxes.shape[0]
         part_box_areas = (part_boxes[:, 3] - part_boxes[:, 1]) * (part_boxes[:, 2] - part_boxes[:, 0]) # Tensor[N2]
-
+        # part_box_areas = torch.where(part_box_areas > 0, part_box_areas, torch.ones_like(part_box_areas)) # to avoid division by 0 in computing part_overlap_fraction
+        
         # top-left point of interection area corresponding to each part-object pair
         temp = torch.stack([obj_boxes[:, :2].contiguous().view(-1).repeat(N2), part_boxes[:, :2].repeat(1, N1).view(-1)], dim=1)
         intersect_top_left = temp.max(dim=1)[0].view(N2, N1, 2)
@@ -289,18 +290,19 @@ class JointDetector(nn.Module):
         related_part_obj_pair_scores = (part_overlap_fraction >= self.fusion_thresh).float() # 0/1 score
 
         if self.use_attention:
-            part_obj_idxs = torch.nonzero(related_part_obj_pair_scores > 0) # Tensor[N_part_obj_pairs, 2]
+            part_obj_idxs = torch.nonzero(part_overlap_fraction >= self.fusion_thresh) # Tensor[N_part_obj_pairs, 2]
             attn_input = torch.cat([obj_box_features[part_obj_idxs[:,1]], part_box_features[part_obj_idxs[:,0]]], dim=1) # Tensor[N_part_obj_pairs, in_features]
             attn_scores = self.attention_layer(attn_input) # Tensor[N_part_obj_pairs, 1] scores for each related part-object pair
-            related_part_obj_pair_scores[related_part_obj_pair_scores > 0] = attn_scores.view(-1) # use score instead of 1
+            related_part_obj_pair_scores = related_part_obj_pair_scores.clone()
+            related_part_obj_pair_scores[part_overlap_fraction >= self.fusion_thresh] = attn_scores.view(-1) # use score instead of 1
             ## NOTE: if memory is an issue, attn_scores can be computed in batches of attn_inputs
 
         part_wise_obj_overlaps = related_part_obj_pair_scores.sum(dim=1) # Tensor[N2]
-        part_wise_obj_overlaps[part_wise_obj_overlaps == 0.0] = 1.0 # to avoid division by 0 while taking mean for parts not overlapping with any obj
+        part_wise_obj_overlaps = torch.where(part_wise_obj_overlaps > 0, part_wise_obj_overlaps, torch.ones_like(part_wise_obj_overlaps))
         related_obj_box_feats = torch.matmul(related_part_obj_pair_scores, obj_box_features) / part_wise_obj_overlaps.view(N2, 1)
         
         obj_wise_part_overlaps = related_part_obj_pair_scores.sum(dim=0) # Tensor[N1]
-        obj_wise_part_overlaps[obj_wise_part_overlaps == 0.0] = 1.0 # to avoid division by 0 while taking mean for obj not overlapping with any parts
+        obj_wise_part_overlaps = torch.where(obj_wise_part_overlaps > 0, obj_wise_part_overlaps, torch.ones_like(obj_wise_part_overlaps))
         related_part_box_feats = torch.matmul(related_part_obj_pair_scores.t(), part_box_features) / obj_wise_part_overlaps.view(N1, 1)
 
         return related_part_box_feats, related_obj_box_feats